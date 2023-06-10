ce71f3b8cd53297771b1b64f44361276496e7703
haruoSugano <haruosugano@icloud.com>
2023-06-09 21:49:09 -0300

    feat: adicionando validacao ao formulario

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,7 +12,8 @@ export function Routes() {
     return (
         <Box flex={1} bg="gray.700">
             <NavigationContainer>
-                <AppRoutes />
+                <AuthRoutes />
+                {/* <AppRoutes /> */}
             </NavigationContainer>
         </Box>
     );
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,5 +1,8 @@
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 import { useNavigation } from "@react-navigation/native";
+import { useForm, Controller } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
@@ -7,14 +10,36 @@ import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
+type FormDataProps = {
+    name: string;
+    email: string;
+    password: string;
+    passwordConfirm: string;
+}
+
+const signUpSchema = yup.object({
+    name: yup.string().required("Informe o nome."),
+    email: yup.string().required("Informe o e-mail.").email("E-mail inválido."),
+    password: yup.string().required("Informe a senha.").min(6, "A senha deve ter pelo menos 6 digitos."),
+    passwordConfirm: yup.string().required("Confirme a senha.").oneOf([yup.ref('password'), ""], "A confirmação da senha não confere")
+});
+
 export function SignUp() {
 
+    const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
+        resolver: yupResolver(signUpSchema)
+    });
+    
     const navigation = useNavigation();
 
     function handleGoBack() {
         navigation.goBack();
     }
-    
+
+    function handleSignUp(data: FormDataProps) {
+        console.log(data);
+    }
+
     return (
         <ScrollView
             contentContainerStyle={{ flexGrow: 1 }}
@@ -42,23 +67,67 @@ export function SignUp() {
                         Crie sua conta
                     </Heading>
 
-                    <Input
-                        placeholder="E-mail"
-                        keyboardType="email-address"
-                        autoCapitalize="none"
+                    <Controller
+                        control={control}
+                        name="name"
+                        render={({ field: { onChange, value } }) => (
+                            <Input
+                                placeholder="Nome"
+                                onChangeText={onChange}
+                                value={value}
+                                errorMessage={errors.name?.message}
+                            />
+                        )}
                     />
 
-                    <Input
-                        placeholder="Nome"
+                    <Controller
+                        control={control}
+                        name="email"
+                        render={({ field: { onChange, value } }) => (
+                            <Input
+                                placeholder="E-mail"
+                                keyboardType="email-address"
+                                autoCapitalize="none"
+                                onChangeText={onChange}
+                                value={value}
+                                errorMessage={errors.email?.message}
+                            />
+                        )}
                     />
 
-                    <Input
-                        placeholder="Senha"
-                        secureTextEntry
+                    <Controller
+                        control={control}
+                        name="password"
+                        render={({ field: { onChange, value } }) => (
+                            <Input
+                                placeholder="Senha"
+                                secureTextEntry
+                                onChangeText={onChange}
+                                value={value}
+                                errorMessage={errors.password?.message}
+                            />
+                        )}
+                    />
+
+                    <Controller
+                        control={control}
+                        name="passwordConfirm"
+                        render={({ field: { onChange, value } }) => (
+                            <Input
+                                placeholder="Confirme a senha"
+                                secureTextEntry
+                                onChangeText={onChange}
+                                value={value}
+                                onSubmitEditing={handleSubmit(handleSignUp)}
+                                returnKeyType="send"
+                                errorMessage={errors.passwordConfirm?.message}
+                            />
+                        )}
                     />
 
                     <Button
                         title="Criar e acessar"
+                        onPress={handleSubmit(handleSignUp)}
                     />
                 </Center>
 
